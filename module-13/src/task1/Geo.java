47401e094eb065782ee5975dae56acfd5c615efd
vadim shevikov <vadim.shevikov03@gmail.com>
2023-08-21 19:15:37 +0300

    fix hw13

--- module-13/src/task1/Geo.java
+++ module-13/src/task1/Geo.java
@@ -1,9 +1,23 @@
 package task1;
 
-import lombok.Data;
 
-@Data
 public class Geo {
     String lat;
     String lng;
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
 }
--- module-13/src/task1/User.java
+++ module-13/src/task1/User.java
@@ -1,8 +1,5 @@
 package task1;
 
-import lombok.Data;
-
-@Data
 public class User {
     int id;
     String name;
@@ -12,4 +9,68 @@ public class User {
     String phone;
     String website;
     Company company;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 }
